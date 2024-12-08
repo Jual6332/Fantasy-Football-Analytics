de7f956d107febec2bbf1a086301d053b2219f05
Justin Alvey <jual6332@colorado.edu>
2024-12-08 14:55:40 -0500

    Implemented these new classes into main function

--- code/main.py
+++ code/main.py
@@ -1,4 +1,24 @@
+from player import *
+from injury import *
+
 def loadWRData():
     pass
 
-loadWrData()
+def compare2Players():
+    # First check if each player has an injury history
+    pass
+
+#loadWrData()
+
+ar = Player()
+ar.set_name("Aaron Rodgers")
+
+inj1 = Injury()
+inj1.set_description("Aaron Rodgers tore his Achilles in the first game of Jets season")
+inj1.set_year(2023)
+inj1.set_severity("Major")
+
+print(ar.get_name())
+
+
+# Did this player overachieve or underachieve based on where he was drafted?
